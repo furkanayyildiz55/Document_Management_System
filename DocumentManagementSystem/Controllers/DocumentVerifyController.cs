2514abaf60b9cfe06b56dc392b65a6d298c82e69
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-02-21 23:37:20 +0300

    21.02.2023

---
+++ DocumentManagementSystem/Controllers/DocumentVerifyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentManagementSystem.Controllers
+{
+    public class DocumentVerifyController : Controller
+    {
+        // GET: DocumentVerify
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
