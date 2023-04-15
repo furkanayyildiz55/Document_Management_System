e580fc272372384d765ecf6b7635bcd9cff71eba
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-04-16 00:04:22 +0300

    16.04.2023

--- DocumentManagementSystem/Controllers/HomeController.cs
+++ DocumentManagementSystem/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using DocumentManagementSystem.Models;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +15,7 @@ namespace DocumentManagementSystem.Controllers
     public class HomeController : Controller
     {
 
+        DocumentManager documentManager = new DocumentManager(new EfDocumentDal());
 
 
         public ActionResult SignIn()
@@ -18,7 +25,33 @@ namespace DocumentManagementSystem.Controllers
 
         public ActionResult Home()
         {
-            return View();
+            DocumentVerificationModel documentVerificationModel = new DocumentVerificationModel();
+            documentVerificationModel.isPostMethod = false;
+            return View( documentVerificationModel);
+        }
+
+        [HttpPost]
+        public ActionResult Home(DocumentVerificationModel documentVerificationModel)
+        {
+            DocumentVerificationValidator documentVerificationValidator = new DocumentVerificationValidator();
+            Document ValidationDocument = new Document();
+            ValidationDocument.DocumentVerificationCode = documentVerificationModel.VerificationCode;
+            ValidationResult result = documentVerificationValidator.Validate(ValidationDocument);
+
+            if (result.IsValid)
+            {
+                documentVerificationModel.document = documentManager.GetDocumentWithVerificationCode(documentVerificationModel.VerificationCode);
+                documentVerificationModel.isPostMethod = true;
+            }
+            else
+            {
+                //foreach (var item in result.Errors)
+                //{
+                //    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                //}
+            }
+            documentVerificationModel.isPostMethod = true;
+            return View(documentVerificationModel);
         }
 
 
